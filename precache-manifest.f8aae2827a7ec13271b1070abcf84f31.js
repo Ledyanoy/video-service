d279b3e9ff9e2c74a36a9e8a9d5066be99cf66d8
korenev <ledaynoy44@hmail.com>
2020-10-21 11:01:54 +0400

    Updates

---
+++ precache-manifest.f8aae2827a7ec13271b1070abcf84f31.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57baf886cdb63fc2ee35e0e7e430cfc1",
+    "url": "/video-service/index.html"
+  },
+  {
+    "revision": "a6c7f0de4463ddc0ed0f",
+    "url": "/video-service/static/css/main.8f35851c.chunk.css"
+  },
+  {
+    "revision": "00d597364887684bcf92",
+    "url": "/video-service/static/js/2.3eb74fd6.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/video-service/static/js/2.3eb74fd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6c7f0de4463ddc0ed0f",
+    "url": "/video-service/static/js/main.3f365746.chunk.js"
+  },
+  {
+    "revision": "c6a5767cae13a2ea3ae2",
+    "url": "/video-service/static/js/runtime-main.809199f3.js"
+  },
+  {
+    "revision": "348a37ff9f2fdd15c5dc999cf1c010ba",
+    "url": "/video-service/static/media/Rubik-Bold.348a37ff.woff2"
+  },
+  {
+    "revision": "a36cb4cc1dd2fa98f58d9b2a5bb33a36",
+    "url": "/video-service/static/media/Rubik-Bold.a36cb4cc.woff"
+  },
+  {
+    "revision": "ad6253f2770a69e30d6516b7b753cf0c",
+    "url": "/video-service/static/media/Rubik-Bold.ad6253f2.eot"
+  },
+  {
+    "revision": "457e61c034561dcf61bc40dba0647c2b",
+    "url": "/video-service/static/media/Rubik-Medium.457e61c0.woff2"
+  },
+  {
+    "revision": "6a640e682468a513e1b27cb24e4a5503",
+    "url": "/video-service/static/media/Rubik-Medium.6a640e68.woff"
+  },
+  {
+    "revision": "e2d2e8906b7368feaa248d67eac16f77",
+    "url": "/video-service/static/media/Rubik-Medium.e2d2e890.eot"
+  },
+  {
+    "revision": "2f41165959c6d808c7a326881f53c249",
+    "url": "/video-service/static/media/Rubik-Regular.2f411659.woff"
+  },
+  {
+    "revision": "c81ba396465b775c34eab9437cdabef3",
+    "url": "/video-service/static/media/Rubik-Regular.c81ba396.eot"
+  },
+  {
+    "revision": "d57d4c613b7a4e76b61040d1a3d712f2",
+    "url": "/video-service/static/media/Rubik-Regular.d57d4c61.woff2"
+  },
+  {
+    "revision": "7cedf79909ab02691a628f14dac57cab",
+    "url": "/video-service/static/media/bat.7cedf799.png"
+  },
+  {
+    "revision": "11c6ce1fd7ec39ea143388a2e0215944",
+    "url": "/video-service/static/media/cat.11c6ce1f.png"
+  },
+  {
+    "revision": "efc916ff7202064679019469341d0720",
+    "url": "/video-service/static/media/htc.efc916ff.svg"
+  },
+  {
+    "revision": "ff16ec5d48786e9a5fa1a80c7d74a0bb",
+    "url": "/video-service/static/media/logo.ff16ec5d.svg"
+  },
+  {
+    "revision": "6fb2ff7c81b1e6d77a29abbabfc8d735",
+    "url": "/video-service/static/media/once.6fb2ff7c.png"
+  },
+  {
+    "revision": "48330fa1b56cad87c1e9644adbc7ce7c",
+    "url": "/video-service/static/media/strip.48330fa1.png"
+  }
+]);
